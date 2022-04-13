6b2abd9d233148864b5546f4e5bf6f1c88e46f6e
ChernitskayaD <72160227+ChernitskayaD@users.noreply.github.com>
2022-04-13 12:20:15 +0300

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorpCalendar
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmployeeInfoForm newForm = new EmployeeInfoForm();
+            newForm.textBox1.Text = this.button1.Text;
+            newForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EmployeeInfoForm newForm = new EmployeeInfoForm();
+            newForm.textBox1.Text = this.button2.Text;
+            newForm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EmployeeInfoForm newForm = new EmployeeInfoForm();
+            newForm.textBox1.Text = this.button3.Text;
+            newForm.Show();
+        }
+    }
+}
